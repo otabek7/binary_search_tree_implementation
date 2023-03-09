b76aaee7fcf33696134ab41b3d2822eaa316d9e8
mavlonovo <mavlonovo@tamu.edu>
2023-03-09 08:52:54 -0600

    third_commit

--- leyk-csce221-assignment-binary-search-tree-main/src/BinarySearchTree.h
+++ leyk-csce221-assignment-binary-search-tree-main/src/BinarySearchTree.h
@@ -53,7 +53,7 @@ public:
     BinarySearchTree(const BinarySearchTree &rhs)
     {
         // TODO
-        _root = rhs._root;
+        _root = clone(rhs._root);
         _size = rhs._size;
     }
     BinarySearchTree(BinarySearchTree &&rhs)
@@ -76,7 +76,6 @@ public:
     {
         // TODO
         return (_root)->element;
-
     }
     bool contains(const key_type &x) const { return contains(x, _root); }
     value_type &find(const key_type &key) { return find(key, _root)->element.second; }
@@ -104,49 +103,76 @@ public:
     BinarySearchTree &operator=(const BinarySearchTree &rhs)
     {
         // TODO
+        if (this != &rhs)
+        {
+            clear();
+            _root = clone(rhs._root);
+            // comp = this->comp;
+            _size = rhs._size;
+        }
+        return *this;
     }
     BinarySearchTree &operator=(BinarySearchTree &&rhs)
     {
         // TODO
+        if (this != &rhs)
+        {
+            clear();
+            _root = rhs._root;
+            _size = rhs._size;
+            // comp = std::move(rhs.comp);
+
+            rhs._root = nullptr;
+            rhs._size = 0;
+        }
+
+        return *this;
     }
 
 private:
     void insert(const_reference x, node_ptr &t)
     {
         // TODO
-        if(t == nullptr){
+        if (t == nullptr)
+        {
             t = new node(x, nullptr, nullptr);
             ++_size;
         }
 
-        if(comp(x.first, t->element.first)){
+        else if (comp(x.first, t->element.first))
+        {
             insert(x, t->left);
         }
-        
-        if(comp(t->element.first, x.first)){
+
+        else if (comp(t->element.first, x.first))
+        {
             insert(x, t->right);
         }
-        else{
+        else
+        {
             t->element.second = x.second;
         }
-
     }
     void insert(pair &&x, node_ptr &t)
     {
         // TODO
-        if(t == nullptr){
+        if (t == nullptr)
+        {
             t = new node(std::move(x), nullptr, nullptr);
             ++_size;
         }
 
-        if(comp(x.first, t->element.first)){
+        else if (comp(x.first, t->element.first))
+        {
             insert(std::move(x), t->left);
         }
-        
-        if(comp(t->element.first, x.first)){
+
+        else if (comp(t->element.first, x.first))
+        {
             insert(std::move(x), t->right);
         }
-        else{
+        else
+        {
             t->element.second = std::move(x.second);
         }
     }
@@ -154,36 +180,144 @@ private:
     void erase(const key_type &x, node_ptr &t)
     {
         // TODO
+        if (t == nullptr)
+            return;
+        else if (comp(x, t->element.first))
+        {
+            erase(x, t->left);
+        }
+
+        else if (comp(t->element.first, x))
+        {
+            erase(x, t->right);
+        }
+        else
+        {
+            // Case 1: No Child
+
+            if (t->left == nullptr && t->right == nullptr)
+            {
+                delete t;
+                t = nullptr;
+                _size--;
+            }
+            // Case 2: One Child
+            else if (t->left == nullptr)
+            {
+                BinaryNode *temp = t;
+                t = t->right;
+                delete temp;
+                _size--;
+            }
+            else if (t->right == nullptr)
+            {
+                BinaryNode *temp = t;
+                t = t->left;
+                delete temp;
+                _size--;
+            }
+            // Case 3: Two Children
+            else
+            {
+                t->element = min(t->right)->element;
+                erase(min(t->right)->element.first, t->right);
+            }
+        }
     }
 
     const_node_ptr min(const_node_ptr t) const
     {
         // TODO
+        // BinaryNode *current = t;
+        // if (t == nullptr)
+        //     return nullptr;
+        // else
+        // {
+        //     if()
+
+        //     while (t->left != nullptr) // should it be current->left->second?
+        //     {
+        //         current = current->left;
+        //     }
+        // }
+        // return (current);
+
+        if (t == nullptr)
+            return nullptr;
+        if (t->left == nullptr)
+            return t;
+        else
+            min(t->left);
     }
     const_node_ptr max(const_node_ptr t) const
     {
         // TODO
+        if (t == nullptr)
+            return nullptr;
+        if (t->right == nullptr)
+            return t;
+        else
+            max(t->right);
     }
 
     bool contains(const key_type &x, const_node_ptr t) const
     {
         // TODO
+        if (t == nullptr)
+            return false;
+        else if (comp(t->element.first, x))
+            return contains(x, t->right);
+        else if (comp(x, t->element.first))
+            return contains(x, t->left);
+
+        return true;
     }
     node_ptr find(const key_type &key, node_ptr t)
     {
         // TODO
+        if (t == nullptr)
+            return nullptr;
+
+        if (comp(key, t->element.first))
+        {
+            return find(key, t->left);
+        }
+
+        else if (comp(t->element.first, key))
+        {
+            return find(key, t->right);
+        }
+
+        else
+            return t;
     }
     const_node_ptr find(const key_type &key, const_node_ptr t) const
     {
         // TODO
+
+        if (t == nullptr)
+            return nullptr;
+
+        if (comp(key, t->element.first))
+        {
+            return find(key, t->left);
+        }
+
+        else if (comp(t->element.first, key))
+        {
+            return find(key, t->right);
+        }
+
+        else
+            return t;
     }
 
     void clear(node_ptr &t)
     {
         // TODO
-        if(t == nullptr)
+        if (t == nullptr)
             return;
-        clear (t->right);
+        clear(t->right);
         clear(t->left);
         delete t;
         t = nullptr;
@@ -192,6 +326,9 @@ private:
     node_ptr clone(const_node_ptr t) const
     {
         // TODO
+        if (t != nullptr)
+            return new node(t->element, clone(t->left), clone(t->right));
+        return nullptr;
     }
 
 public:
